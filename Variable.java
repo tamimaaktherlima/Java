991ffa1fa6501dfb7a188a59a125dfd330635dd3
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-05-06 22:55:33 +0600

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,30 @@
+public class Variable {
+    public static void main(String[] args){
+
+        // Dynamic Variable Initialization
+        int m = 254163;
+        System.out.println(m);
+
+        // variable Declareation
+        int n;
+        float f;
+        char c;
+        boolean b;
+        double d;
+        
+        // Value Initialization
+        b = true;
+        c = 'A';
+        n = 524;
+        d = 253.1452;  // Java te by default float number gulo double ei thake
+        f = 20.54f;
+
+        // Printing the value
+        System.out.println("boolean = "+b);
+        System.out.println("Integer = "+n);
+        System.out.println("Double = "+d);
+        System.out.println("Character = "+c);
+        System.out.println("Float = "+f);
+
+    }
+}
