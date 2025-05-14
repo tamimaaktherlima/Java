fbb2903b3ee85f8204062178558a74f8f8183c1b
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-05-14 22:51:56 +0600

    For Loop

---
+++ For_loop.java
@@ -0,0 +1,9 @@
+public class For_loop {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        for(i=1;i<=5;i++){
+            System.out.println("Java");
+        }
+    }
+}
