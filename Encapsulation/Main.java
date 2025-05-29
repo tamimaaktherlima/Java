060c264b3f5a499f0798cc3b2490062d4d689c95
Tamima Akther Lima <168561062+tamimaaktherlima@users.noreply.github.com>
2025-05-29 23:39:40 +0600

    Encapsulation
    Data hiding

---
+++ Encapsulation/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        p.setName("Tamima Akther Lima");
+        System.out.println(p.getName());
+
+        p.setAge(25);
+        System.out.println(p.getAge());
+    }
+}
---
+++ Encapsulation/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    // Encapsulation --> Data hiding. 
+    private String name;
+    private int age;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+}
